c7ce24ea70c32b435674ca85539e9c87d3306393
Sandhya <subramaniyansandhya@gmail.com>
2020-06-12 22:14:28 +0530

    Updates

---
+++ precache-manifest.d58515ef6c550251622842812a905baf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22a0f052ad7176237129282442c035a",
+    "url": "../build/index.html"
+  },
+  {
+    "revision": "27d18ef42d38126e0963",
+    "url": "../build/static/css/main.9df45d85.chunk.css"
+  },
+  {
+    "revision": "14521de10edf7a52baa4",
+    "url": "../build/static/js/2.1c54f9d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "../build/static/js/2.1c54f9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d18ef42d38126e0963",
+    "url": "../build/static/js/main.ed8fd687.chunk.js"
+  },
+  {
+    "revision": "9c479389eb4b4477808a",
+    "url": "../build/static/js/runtime-main.45851ec2.js"
+  },
+  {
+    "revision": "5ba473168743048df5d5baa99867f751",
+    "url": "../build/static/media/banner.5ba47316.jpg"
+  }
+]);
