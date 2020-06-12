f82ed95fcc9ed22796cde67f4bb2922037928bfc
Gokul Ramesh <gokul.ramesh@siriuscom.com>
2020-06-12 21:03:56 +0530

    Updates

---
+++ precache-manifest.2622a52ab1fcb0530b49f8de1bc11c0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15b9149a6d791a7b7016653dad083fcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3948f088047b59029e4b",
+    "url": "/static/css/main.39df9d16.chunk.css"
+  },
+  {
+    "revision": "14521de10edf7a52baa4",
+    "url": "/static/js/2.1c54f9d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1c54f9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3948f088047b59029e4b",
+    "url": "/static/js/main.6461fb06.chunk.js"
+  },
+  {
+    "revision": "b4ba650b238755144e2a",
+    "url": "/static/js/runtime-main.e5fc83a6.js"
+  },
+  {
+    "revision": "5ba473168743048df5d5baa99867f751",
+    "url": "/static/media/banner.5ba47316.jpg"
+  }
+]);
