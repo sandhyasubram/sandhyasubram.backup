43e29bf841f6ae2980b725917a42025ca5bfcac0
Sandhya <subramaniyansandhya@gmail.com>
2020-06-13 21:41:25 +0530

    Banner changes

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -7,7 +7,9 @@ const Banner = props => {
     <div className="banner-wrapper">
       <div
         className="banner-image"
-        style={{ backgroundImage: `url(${props.image})` }}
+        style={{ backgroundImage: `radial-gradient(circle at top left,
+          rgba(30, 0, 20, 0.5),
+          rgba(0, 0, 0, 0.5)),url(${props.image})`}}
       >
         <div className="banner-text">
           {props.bannerText.map((text, index) => {
