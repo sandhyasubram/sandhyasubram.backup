2fb4c21679adb4edf19f8d2e75381ffdba4a8ce4
Sandhya <subramaniyansandhya@gmail.com>
2020-06-18 12:47:29 +0530

    links and home changes

--- src/constants/Strings.js
+++ src/constants/Strings.js
@@ -10,15 +10,14 @@ const Strings = {
         BANNER_TEXT: {
           GREETING: "Hi",
           NAME: "I'm Sandhya"
-        },
-        INTRO_TEXT: "A UX Designer who goes by @tangentkitty on the web"
+        }
       }
     },
     SOCIAL: [
       { link: "https://dribbble.com/sandhya_Subram", icon: "dribbble" },
-      { link: "https://www.behance.net/tangentkitty", icon: "behance" },
+      { link: "https://www.behance.net/sandhya_subram", icon: "behance" },
       { link: "https://codepen.io/sandhya_subram/", icon: "codepen" },
-      { link: "https://www.linkedin.in/in/tangentkitty/", icon: "linkedin" },
+      { link: "https://www.linkedin.in/in/sandhyasubram/", icon: "linkedin" },
       { link: "https://twitter.com/sandhya_subram", icon: "twitter" },
       { link: "https://www.instagram.com/tangentkitty/", icon: "instagram" }
     ]
