42c75baf8089f027d7620148584ee9bbba67baed
Sandhya <subramaniyansandhya@gmail.com>
2020-11-23 18:46:06 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c1b32f1c1f6fad6903314fce0c98f938.js"
+  "/precache-manifest.7c7ca242b2de6c1e6c88aa1cffd98700.js"
 );
 
 self.addEventListener('message', (event) => {
